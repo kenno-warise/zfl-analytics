4e2de0152703e13dadff2cd4b08bd49ac2e57f17
Kenno-Warise <wariken0523@gmail.com>
2024-03-04 19:02:15 +0900

    アジャックスの非同期処理によるアナリティクスデータをテーブルに反映させる

--- analytics/urls.py
+++ analytics/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 
+app_name = 'analytics'
+
 urlpatterns = [
-        path('', views.HomePageView.as_view(), name='index')
+        path('', views.HomePageView.as_view(), name='index'),
+        path('pulldown/', views.pulldown, name='pulldown')
 ]
 
