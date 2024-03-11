88e76405a07763b07e8c3639631567ab20a6a339
Kenno-Warise <wariken0523@gmail.com>
2024-03-11 19:40:17 +0900

    管理ページトップのレイアウトを変更する

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 
 from .models import AnalyticsAppSettings, GoogleAnalytics4Config, DimensionDate
 
 
-admin.site.register(AnalyticsAppSettings)
+class AnalyticsAppSettingsAdmin(admin.ModelAdmin):
+    save_on_top = True
+    fieldsets = (
+            ("ベースHTMLファイル設定", {
+                "fields": ("base_html_file",)
+            }),
+            ("ページの編集", {
+                "fields": ("nav_title", "heading_ja", "heading_us")
+            }),
+            ("レイアウトの調整", {
+                "fields": ("container", ("col_left", "col_center", "col_right"),)
+            }),
+    )
+
+
+admin.site.register(AnalyticsAppSettings, AnalyticsAppSettingsAdmin)
 admin.site.register(GoogleAnalytics4Config)
 admin.site.register(DimensionDate)
 
